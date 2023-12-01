ad1051ee4d3d8c88f29f8aba381b29667d13b988
Kekus <muzanska@yandex.ru>
2023-12-01 18:52:13 +0400

    Sprint_6

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,18 +5,19 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
+    final Feline feline;
+    //  Класс Lion не должен зависеть от класса Feline. Использован принцип инъекции зависимостей.
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
     }
-
-    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
